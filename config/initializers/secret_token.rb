d4e1331133f08e569a9328490fb2c66514951833
Oleg Pasko <olegpasko@gmail.com>
2012-07-08 09:43:40 +0300

    init with products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evershop::Application.config.secret_token = '1604be5730f60585917b27881de88ec8e11f4ef35da3f9132b4b7408c2fdc09e956e64e7c3349c26148f202f091b87fa4a917113acba54a87e8a15dd4abc1409'
