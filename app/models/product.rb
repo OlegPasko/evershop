e8d3f6ef2c9b5b0fbb84d2f63a0ec48f12a3cf5a
Oleg Pasko <olegpasko@gmail.com>
2012-07-13 15:56:44 +0300

    basic tests for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :price, :title
+  
+  validates_presence_of :description, :price, :title
+  validates_uniqueness_of :title
+  validates :price, numericality: {greater_than_or_equal_to: 0}
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  test "attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+  end
+  test "price must not be negative" do
+    product = Product.new(title: 'Titleus', description: 'some description')
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0",
+    product.errors[:price].join('; ')
+    product.price = 1
+    assert product.valid?
+    product.price = 0
+    assert product.valid?
+  end
 end
